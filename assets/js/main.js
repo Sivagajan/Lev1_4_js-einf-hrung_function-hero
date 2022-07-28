c9214a80cbf33c8eb95a78eca2c8b590d78d408a
Sivagajan <sivagajan@gmail.com>
2022-07-28 13:47:36 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log('it works')
+
+let name = 'Mein Lieblingsheld aus Marvel ist: '
+let power = ' Er hat die Fähigkeit: '
+let enemy = ' Sein größter Gegner ist: '
+
+function hero(heroName, heroPower, heroEnemy){
+    console.log(name + heroName + power + heroPower + enemy + heroEnemy)
+}
+
+hero('Superman', 'Unverwundbarkeit','Lex Luthor')
+
